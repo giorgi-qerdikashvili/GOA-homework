d3d819f3eff14c5bfbf467a4d068dff16fd69b92
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-07-02 19:54:43 +0400

    second commit

---
+++ level 01/homework/homework.py
@@ -0,0 +1,13 @@
+#ჩვენ ვისწავლეთ 4 სახის მონაცემთა ტიპი: string = "სტრინგი" , integer = მთელი რიცხვები, float = წილადი რიცხვები და boolean რომელიც არის True or False
+#string = "nika" , "giorgi", "12334" და ასე შემდეგ
+#integer = 1 ,13,425, 2345 და აშ.
+#float = 1.5, 254.35 , 0.2 და აშ.
+#bool = True or False
+print("მარი")
+print(32)
+print(3.5)
+print(True)
+# variable არის ყუთი რომელიც როდესაც ვიძახებთ კომპიუტერს აღიქვამს ამ ვარიაბელს როგორც მის შიგთავსს
+name = "giorgi"
+age = 19
+height = 1.8
