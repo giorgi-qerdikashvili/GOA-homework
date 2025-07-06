998323721afd54133d4182c7935e64ab9fd673bf
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-07-06 17:10:38 +0400

    third commit

---
+++ level 02/classwork/classwork.py
@@ -0,0 +1,11 @@
+num1 = 2
+num = 3
+print(num1 + num)
+jami = num + num1
+gamokleba = num - num1
+gamravleba = num1*num
+gayofa = num / num1
+print(gamokleba)
+print(jami)
+print(gamravleba)
+print(gayofa)
