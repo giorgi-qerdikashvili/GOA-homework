cd15ae96d7d12d434fb7276506d12d25305b7234
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-07-11 15:57:33 +0400

    forth commit

---
+++ level 03/classwork/classwork.py
@@ -0,0 +1,41 @@
+name = "თევზი"
+name = "თემური"
+print(name)
+saxeli = "petre"
+Saxeli = "pavle"
+SAxeli = "zaza"
+SAXeli = "zura"
+SAXEli = "temuri"
+SAXEli = "temuri"
+
+int_1 = 23
+int_2 = 45
+int_3 = 32
+int_4 = 422
+
+big_name = "lado"
+Big_name = "kaxa"
+BIG_name = "ramaz"
+BIG_NAME = "teimuraz"
+
+float_1 = 1.0
+Float_1 = 11.2
+FLOAT_1 = 22.46
+floaT_1 = 12.1
+
+print(float_1)
+print(Float_1)
+print(FLOAT_1)
+print(floaT_1)
+print(int_1)
+print(int_2)
+print(int_3)
+print(int_4)
+print(big_name)
+print(Big_name)
+print(BIG_NAME)
+print(BIG_NAME)  
+user%name = "goga" # უნდა იყოს _ პროცენტის ნაცვლად
+user surname = "chalauri" #უნდა იყოს _ სპეისის ნაცვლად
+first-number = 200 #უნდა იყოს _ ტირეს ნაცვლად
+second.number = 123 #უნდა იყოს _ წერტილის ნაცვლად
---
+++ level 03/homework/homework.py
@@ -0,0 +1,42 @@
+num_0 = 3
+num_1 = 10
+sum_of = num_0 + num_1
+min_of = num_0 - num_1
+mult_of = num_0 * num_1 
+dev_of = num_1 / num_0
+
+
+name = "giorgi"
+name = "kaxa"
+name = "nikoloz"
+name = "mariam"
+name = "lizi"
+print(name)
+
+name = "liz"
+Name = "nika"
+nAme = "lika"
+NAme = "nia"
+NAMe = "nana"
+print(name)
+print(Name)
+print(nAme)
+print(NAme)
+print(NAMe)
+
+name2 = "giorgi"              #ვარიებელი არ უნდა იწყებოდეს ციფრით
+user_name = "bubunauri"       # {-ს ნაცვლად უნდა იყოს _
+user_name = "goga"            #გოგა უნდა იყოს ბრჭყალებში
+user_surname = "axalaia"      #ტირეს ნაცვლად უნდა იყოს უნდერსქორი და ახალაია უნდა იყოს ბრჭყალებში
+
+name_0 = "luka"
+name_1 = "niko"
+name_2 = "tako"
+name_3 = "mako"
+name_4 = "ikako"
+print(name_0 + name_1 + name_2 + name_3 + name_4)  #string ების შეერთებას კონკატინაცია ქვია
+
+saxeli = "giorgi"
+print (saxeli*10)
+
+# ყველა ოპერაცია გამრავლების გარდა გვაძლევს typeerror-ს გამრავლება კი სტრინგს n რაოდენობით გამოიტანს
