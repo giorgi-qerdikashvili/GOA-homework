04ddb76eb3dbe1fc346b828b88a8c7cebfb2945f
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-07-16 16:00:02 +0400

    sixth commit

---
+++ level 05/homework/homework.py
@@ -0,0 +1,44 @@
+#ჩვენ ვისწავლეთ ენდი და ორი ოპერატორები ასევე ვისწავლეთ შედარების ოპერატორები რომელიც გვეხმარება ციფრების და მნიშვნელობების შედარებაში
+
+num1 = int(input("num1 is: "))
+num2 = int(input("num2 is: "))
+print("num1 > num2: ",num1>num2)
+print("num1 < num2: ",num1<num2)
+print("num1 = num2: ",num1==num2)
+
+n = input("string 1 is: ")
+n1 = input("string 2 is: ")
+n2 = input("string 3 is: ")
+n3 = input("string 4 is: ")
+n4 = input("string 5 is: ")
+print(n+n1+n2+n3+n4)
+
+num = int(input("input num1: "))
+num1 = int(input("input num2: "))
+num2 = int(input("input num3: "))
+num3 = int(input("input num4: "))
+print((num + num1 + num2 + num3)/ 4)
+
+value1 = 32
+value2 = "13" 
+value3 = True
+value4 = 1.5
+print(type(value1))
+print(type(value2))
+print(type(value3))
+print(type(value4))
+
+string1 = "132"
+string2 = "132"
+print(string1 == string2)
+
+num1 = "12"
+num2 = "32"
+num3 = "55"
+num4 = "9"
+print(int(num1) + int(num2) + int(num3) + int(num4) )
+
+num1 = 32
+num2 = 43
+num3 = 10
+print(str(num1) + str(num2) + str(num3))
