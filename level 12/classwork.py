b3aa7d3f9dd818dc5e2f035bdf72e4b6eaf04ca6
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-07-30 16:22:16 +0400

    correction

---
+++ level 12/classwork.py
@@ -0,0 +1,18 @@
+1 == 3
+2 == 2
+4 >= 3
+3>4
+22>0
+4<3
+0<1
+11<=33
+88<=6
+3 >= 23
+311 != 6
+30 !=3
+
+print(33 != 3)
+print(11 ==11 )
+print(90 >= 3)
+
+print(True and False or 5 > 10 and 100 / 5 > 10 and "Python" == "Python" or 2 * 3 < 5 or 6 + 7 - 9 < 20 and True and 5 > 5.0 or 10 - 10 < 100)
