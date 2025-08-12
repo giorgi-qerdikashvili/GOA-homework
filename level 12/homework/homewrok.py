69bf1fb008254f93cf5897601269455dca7e1ba3
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-08-12 17:47:34 +0400

    commit

---
+++ level 12/homework/homewrok.py
@@ -0,0 +1,18 @@
+# conditional statements არი if else elif სტაიტმენტი რომლებიც გვეხმარება რეალური პრობლემების გადასაყვეტადი, მათი დახმარებით კომპიუტერს შეუძლია აირჩის პასუხი
+for i in range(50):
+    print("Hello world")
+i = 3
+while i <= 17 :
+    print(i)
+    i = i + 1
+password = None
+correct = "1234"
+while password != correct:
+    password = input()
+    print("access declined")
+print("access granted")
+a = None
+while a != "ძაღლი":
+    a = input()
+    print("შენ ძაღლი არ გყავს")
+print("woaf! waof!")
