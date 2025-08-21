a1ee35d623dc3b43e26fd24b6d17bc508be6459f
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-08-22 01:26:38 +0400

    addition

---
+++ level 15/homework.py
@@ -0,0 +1,25 @@
+num = int(input("give number: "))
+if num > 10:
+    print("num is more than 10 ")
+else:
+    print("num is less than 10 ")
+
+num = input("give num ")
+if num == "15":
+    print("num is equal to 15")
+else :
+    print("num  is not equal to 15")
+
+name = input("say my name ")
+if name == "giorgi":
+    print("your god dame right!")
+else:
+    print("you all know exactly who i am")
+
+for i in range(50,100,5):
+    print(i)
+p = 20
+while p != 50:
+    print(p)
+    p = p + 1
+
