f0947714383fd83562f3da2e6d1684e29621b5a6
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-09-16 20:36:20 +0400

    just on time

---
+++ level 22/classwork/classwork.py
@@ -0,0 +1,18 @@
+namelist = ["lizi", "luka", "lazare", "sandro", "petre", "pavle", "anzori", "tamazi", "tamari", "tiniko"]
+print(namelist[0])
+print(namelist[1])
+print(namelist[2])
+print(namelist[3])
+print(namelist[4])
+print(namelist[5])
+print(namelist[6])
+print(namelist[7])
+print(namelist[8])
+print(namelist[9])
+
+veggie = ["stafilo", "kartofili", "xaxvi", "stafilo", "kombosto" , "warxali"]
+veggie[0] = "viri"
+veggie[3] = "tagvi"
+veggie[5] = "bokveri"
+for i in range(6):
+    print(veggie[i])
