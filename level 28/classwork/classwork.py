68ca646208f45209021af0d76800c80b861eac88
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2025-10-05 19:31:00 +0400

    on time

---
+++ level 28/classwork/classwork.py
@@ -0,0 +1,12 @@
+arr = ["hello", 5, 3.14, True, False, [1, 2, 3, 4, 5]] 
+print(arr[3:6])
+
+list1= ["12", 33, 45,32 ,True, ["33", False], 90, 69,44,1]
+print(list1[5:])
+
+list2 =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(list2[:-11])
+
+list3= ["a","b","c","d","e","f","g","h","i","j","k","l"]
+
+print(list3[2:int(input("give number"))])
---
+++ level 28/homework/homework.py
@@ -0,0 +1,28 @@
+list1=[1,2,3,4,5,6,7,8,9,10]
+print(list1[:5])
+print(list1[7:])
+print(list1[3:8])
+
+l=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,]
+print(l[:-9])
+print(l[-10:])
+print(l[-8:])
+
+what = "თვითმფრინავი"
+print(what[-4:])
+what ="programmer"
+print(what[:3])
+
+week = ["ორშ", "სამშ", "ოთხშ","ხუთშ","პარ","შაბ","კვირ"]
+print(week[:5])
+print(week[-7:-2])
+print(week[5:])
+print(week[-2:])
+
+stat='Python slicing is very powerful'
+print(stat[:6])
+print(stat[7:14])
+print(stat[23:])
+print(stat[-32:-25])
+print(stat[-24:-17])
+print(stat[-8:])
