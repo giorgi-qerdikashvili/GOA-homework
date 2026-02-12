e5b61e7e38c3cb7457e5c64f77d283c3b3a2fe90
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2026-02-12 14:49:14 +0400

    on time

---
+++ level 64/classwork/classwork.py
@@ -0,0 +1,12 @@
+def say_my_name():
+    print('heisenberg')
+say_my_name()
+say_my_name()
+
+def check_event():
+    num = 20
+    if num % 2 == 1:
+        return 'this is odd'
+    elif num % 2 == 0:
+        return 'this is even'
+print(check_event())
