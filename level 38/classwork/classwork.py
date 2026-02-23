834f87eac51ef50a4ad75eae599408eb431f726a
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2026-02-23 21:17:37 +0400

    New

---
+++ level 38/classwork/classwork.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+# 1) def number_to_string(num):
+#     return str(num)# Return a string of the number here!
+# 2)
---
+++ level 38/homework/homework.py
@@ -0,0 +1,24 @@
+# 1)https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+# def repeat_str(repeat, string):
+#     return repeat*string
+#
+# 2)https://www.codewars.com/kata/54ff3102c1bad923760001f3
+# def get_count(sentence):
+#     count = 0
+#     for i in sentence:
+#         if i in 'aeiou':
+#             count += 1
+#     return count
+# 3)https://www.codewars.com/kata/5715eaedb436cf5606000381
+# def positive_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         if i > 0:
+#             sum +=i
+#     return sum
+# 4)https://www.codewars.com/kata/544675c6f971f7399a000e79
+# def string_to_number(s):
+#     return int(s)
+# 5)https://www.codewars.com/kata/523b4ff7adca849afe000035/train/python
+# def greet():
+#     return 'hello world!'
