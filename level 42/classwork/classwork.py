093503d2f239887d5af9e708d64df915069e319b
giorgi-qerdikashvili <giorgi.qerdikashvili05@gmail.com>
2026-02-24 23:41:33 +0400

    on time

---
+++ level 42/classwork/classwork.py
@@ -0,0 +1,33 @@
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+# def no_space(x):
+#     word = ''
+#     for i in x:
+#         if i != " ":
+#             word += i
+#     return word
+
+# https://www.codewars.com/kata/54edbc7200b811e956000556/train/python
+# def count_sheeps(sheep):
+#     return sheep.count(True)
+
+#https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
+# def summation(num):
+#     sum = 0
+#     for i in range(1,num):
+#         sum += i
+#     sum += num
+#     return sum
+# https://www.codewars.com/kata/5b853229cfde412a470000d0/train/python
+# def twice_as_old(d,s):
+#     age = d - s*2
+#     if age <0:
+#         return -age
+#     else:
+#         return age
+# https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/python
+# def number(bus_stops):
+#     amount = 0
+#     for i in bus_stops:
+#         amount += i[0]
+#         amount -= i[1]
+#     return amount
---
+++ level 42/homework/homework.py
@@ -0,0 +1,39 @@
+# https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python
+# def remove_char(s):
+#     word = []
+#     lesser = ''
+#     for i in s:
+#         word.append(i)
+#     word.pop(0)
+#     word.pop(-1)
+#     for i in word:
+#         lesser += i
+#     return lesser
+# https://www.codewars.com/kata/53ee5429ba190077850011d4
+# def double_integer(i):
+#     return i*2
+# https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+# def greet(name):
+#     return "Hello, "+ name +  " how are you doing today?"
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/python
+# def basic_op(o, v1, v2):
+#     if o =='+':
+#         return v1 + v2
+#     elif o =='-':
+#         return v1 - v2
+#     elif o =='*':
+#         return v1 * v2
+#     elif o =='/':
+#         return v1 / v2
+# https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/python
+# def sum_array(a):
+#     sum = 0
+#     for i in a:
+#         sum += i
+#     return sum
+# https://www.codewars.com/kata/583710ccaa6717322c000105/train/python
+# def simple_multiplication(number) :
+#     if number % 2 == 0:
+#         return number * 8
+#     else:
+#         return number * 9
